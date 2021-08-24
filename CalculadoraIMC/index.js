741d5afed235465aa76bbadbe014e44ede55199c
matheuscabral3 <matheusmendescontato81@gmail.com>
2021-08-24 16:20:20 -0300

    Alteração visual para a página

--- CalculadoraIMC/index.js
+++ CalculadoraIMC/index.js
@@ -2,7 +2,23 @@ $(document).ready(function() {
 
     // jQuery methods go here...
     $("#btnCalcular").click(function() {
-        console.log("jQuery carregado !");
+        // console.log("jQuery carregado !");
+        calcularImc();
+        // console.log("jQuery finalizado !");
     });
 
 });
+
+function calcularImc(imc, peso, altura) {
+    //Obter o resultado
+    var imc;
+    var altura = $("#txtAltura").val();
+    var peso = $("#txtPeso").val();
+
+    //Equação necessária para obter o Resultado.
+    //IMC = peso / (altura)²
+    imc = (peso / (altura * altura))
+
+    //Atribuir o valor da variável para o TEXTO do elemento html.
+    $("#txtAreaResult").html(imc);
+}
